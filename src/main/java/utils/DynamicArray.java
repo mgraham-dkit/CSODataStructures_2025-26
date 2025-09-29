f307325736df502f3f1e010b3de808b6e2834634
michelle <michelle.graham@dkit.ie>
2025-09-29 21:21:51 +0100

    feat: add element to dynamic array

--- src/main/java/utils/DynamicArray.java
+++ src/main/java/utils/DynamicArray.java
@@ -9,9 +9,22 @@ public class DynamicArray {
         size = 0;
     }
 
+    public int size(){
+        return size;
+    }
+
     private void ensureCapacity(){
         if(size == data.length){
+            int [] temp = new int[data.length * 2];
 
+            for (int i = 0; i < data.length; i++) {
+                temp[i] = data[i];
+            }
+
+            // OR
+            //System.arraycopy(data, 0, temp, 0, data.length);
+
+            data = temp;
         }
     }
 
