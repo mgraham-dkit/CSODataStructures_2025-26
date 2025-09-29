0f6e22e72cf99c184eb779361031bc3b06462e99
michelle <michelle.graham@dkit.ie>
2025-09-29 21:22:30 +0100

    feat: get / delete from specific position in dynamic array (including validation)

--- src/main/java/utils/DynamicArray.java
+++ src/main/java/utils/DynamicArray.java
@@ -34,4 +34,39 @@ public class DynamicArray {
         data[size] = element;
         size++;
     }
+
+    public int get(int pos){
+        validatePosition(pos);
+
+        return data[pos];
+    }
+
+    private void validatePosition(int pos) {
+        // Check that the specified position exists within the DATA (not just within the array)
+        if(pos < 0 || pos >= size){
+            // If it does not, throw an exception to indicate this
+            throw new IndexOutOfBoundsException("Cannot access position outside boundaries of the list");
+        }
+    }
+
+    public int remove(int pos){
+        // Confirm the specified position exists within the data
+        validatePosition(pos);
+
+        // SAVE value at original position
+        int deleted = data[pos];
+
+        // SHIFT all data back in the array by one space
+        for (int i = pos; i < size-1; i++) {
+            data[i] = data[i+1];
+        }
+
+        // Replace (overwrite) the final space in the data with a "blank"
+        data[size-1] = 0;
+        // Decrease how many elements are in the array
+        size--;
+
+        // Return the value that was deleted from specified position in the array
+        return deleted;
+    }
 }
