190b64e8bf7e8dca75a90bde3ee675cce1e70809
michelle <michelle.graham@dkit.ie>
2025-09-29 21:25:18 +0100

    feat: application to check functionality for basic DynamicArray features

---
+++ src/main/java/introducing_classes/apps/TestDynamicArray.java
@@ -0,0 +1,26 @@
+package introducing_classes.apps;
+
+import utils.DynamicArray;
+
+public class TestDynamicArray {
+    public static void main(String[] args) {
+        int [] sourceInfo = {1, 2, 3, 4, 5, 6};
+        DynamicArray myList = new DynamicArray();
+
+        for(int num: sourceInfo){
+            myList.add(num);
+        }
+
+        for (int i = 0; i < myList.size(); i++) {
+            System.out.print(myList.get(i) + ", ");
+        }
+
+        System.out.println();
+
+        while(myList.size() > 0){
+            System.out.println("Removed: " + myList.remove(0));
+        }
+
+        System.out.println("Final list size: " + myList.size());
+    }
+}
