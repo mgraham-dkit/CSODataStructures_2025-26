a5282fe929f6cf780554bfe6ffbf1deaf2641a9b
michelle <michelle.graham@dkit.ie>
2025-10-20 17:19:25 +0100

    chore: add todo actions for current exercise work

--- src/main/java/utils/LinkedList.java
+++ src/main/java/utils/LinkedList.java
@@ -54,11 +54,16 @@ public class LinkedList {
         size++;
     }
 
-    // todo: size()
-    // todo: get()
-    // todo: isEmpty()
-    // todo: indexOf()
-    // todo: clear()
-    // todo: contains()
-    // todo: set()
+    // todo: size() -> return the size of the list
+    // todo: get() -> take an index and return the value at that index in the list
+    // todo: isEmpty() -> return a boolean indicating if the list is empty
+    // todo: indexOf() -> take a number and return the index at which that number first appears
+    // todo: clear() -> take nothing in and wipe the list of data
+    // todo: contains() -> take a number and return true if the list contains the value, false otherwise
+    // todo: set() -> take in an index at which to replace the current value and a new value with which to replace it
+    //  . This should return the value originally found at the specified index
+    // todo: deduplicate() -> should return a new list containing one instance of each value from current list
+    // todo: join() -> should take in a second list and add its contents onto this list
+    // todo: rotate() -> should take in the number of positions by which the list should be rotated. For example, if
+    //  1 is supplied, it should take the final element from the list and move it to the start of the list
 }
